bfd00b1a00de943e168e53b319bd3ba78ebe3c45
AurelienBonbony <93644747+AurelienBonbony@users.noreply.github.com>
2022-02-06 15:52:37 +0100

    Add files via upload

---
+++ barredevie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="barredevie" tilewidth="80" tileheight="32" tilecount="7" columns="7">
+ <image source="assets/barredevie.png" width="560" height="32"/>
+</tileset>
